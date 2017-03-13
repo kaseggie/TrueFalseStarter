7daeaf58bcf15d4488d4eb8aaf285e760429c03d
kaseggie <kseggie@gmail.com>
2017-03-13 17:34:52 +0100

    Added QuestionAndAnswers struct

---
+++ TrueFalseStarter/QuestionAndAnswers.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionAndAnswers.swift
+//  TrueFalseStarter
+//
+//  Created by Kavan Seggie on 13/03/2017.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
---
+++ TrueFalseStarter/QuestionProvider.swift
@@ -0,0 +1,27 @@
+//
+//  QuestionProvider.swift
+//  TrueFalseStarter
+//
+//  Created by Kavan Seggie on 13/03/2017.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+
+struct QuestionAndAnswers{
+    let question: String
+    let option1: String
+    let option2: String
+    let option3: String
+    let option4: String
+    let answer: Int
+    
+    func isCorrect(givenAnswer: Int) -> Bool {
+        if givenAnswer == self.answer {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+struct 
